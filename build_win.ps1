b482ca4374404dede78f98227664b7c0e5fa029c
JerryCao1985 <caojiayin1985@icloud.com>
2019-01-25 11:11:10 +0800

    Update build_win.ps1

--- build_win.ps1
+++ build_win.ps1
@@ -1,3 +1,3 @@
 md dependencies
 wget http://45.63.123.194/sort_dependencies/win/easy_profiler.zip -OutFile easy_profiler.zip
-Expand-Archive .\easy_profiler.zip -DestinationPath .\dependencies\
+[System.IO.Compression.ZipFile]::ExtractToDirectory( ".\easy_profiler.zip" , ".\dependencies\" )
