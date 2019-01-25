9537687a6cee643ba1160735e92f6c990982f1dc
JerryCao1985 <caojiayin1985@icloud.com>
2019-01-25 12:44:57 +0800

    update

--- build_win.ps1
+++ build_win.ps1
@@ -1,6 +1,4 @@
 get-host
 mkdir dependencies
 wget http://45.63.123.194/sort_dependencies/win/easy_profiler.zip -O easy_profiler.zip
-cd dependencies
-7z e ../easy_profiler.zip
-cd ..
+7z x easy_profiler.zip -o./dependencies
