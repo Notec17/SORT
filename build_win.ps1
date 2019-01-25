dc31fcc449770c3b0210193d56d92d37a80e0524
JerryCao1985 <caojiayin1985@icloud.com>
2019-01-25 10:52:35 +0800

    Update build_win.ps1

--- build_win.ps1
+++ build_win.ps1
@@ -1,12 +1,3 @@
-mkdir dependencies
-wget http://45.63.123.194/sort_dependencies/win/easy_profiler.zip -O easy_profiler.zip
-
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
-Unzip ".\easy_profiler.zip" ".\dependencies\"
+md dependencies
+wget http://45.63.123.194/sort_dependencies/win/easy_profiler.zip -OutFile easy_profiler.zip
+Expand-Archive .\easy_profiler.zip -DestinationPath .\dependencies\
